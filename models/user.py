6c08edb6ffa5471fa859013ecd07ffee4ca8644f
Guillermo Reyes <guillermo_hard@hotmail.com>
2024-01-05 14:37:08 -0600

    Add users model

---
+++ models/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String(30), nullable=False, index=True)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
+    name: Mapped[str] = mapped_column(String(60), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(60), nullable=False)
+    created: Mapped[datetime] = mapped_column(insert_default=func.now())
+    last_connection: Mapped[datetime] = mapped_column(insert_default=func.now())
