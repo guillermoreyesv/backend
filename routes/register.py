7c3d7c938b920dc363cac2961acfe8c6d4b0fa1d
Guillermo Reyes <guillermo_hard@hotmail.com>
2024-01-05 16:18:18 -0600

    add email validation

--- routes/register.py
+++ routes/register.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel, EmailStr, Field
 from models.user import User
 from config.mysql import MySQLDBSingleton
 from sqlalchemy.orm import Session
+import json
+
 
 router = APIRouter()
 class UserAPI(BaseModel):
@@ -14,10 +16,14 @@ class UserAPI(BaseModel):
 
 @router.post("/")
 def register_new_user(user: UserAPI):
-    #user_data = user.model_dump()
     try:
         engine = MySQLDBSingleton().get_database()
         with Session(engine) as session:
+            result = User.user_exists(session, user.email)
+            if result:
+                response = {'message': f'user {user.email} already exists'}
+                return JSONResponse(status_code=status.HTTP_406_NOT_ACCEPTABLE, content=response)
+            
             user.password = hash_password(user.password)
             new_user = User(
                 email= user.email,
@@ -28,9 +34,9 @@ def register_new_user(user: UserAPI):
             session.add(new_user)
             session.commit()
     except Exception as e:
-        response = {"message": "Can't connect to database or start a session"}
         print(e)
-        #return JSONResponse(status_codem=status.HTTP_500_INTERNAL_SERVER_ERROR, content=response)
+        response = {"message": "Can't connect to database or start a session"}
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=response)
 
     return {"message": "User registered successfully", "user_data": user}
 
@@ -39,7 +45,3 @@ def hash_password(password: str):
     password_manager = PasswordManager()
     password = password_manager.hash_password(password)
     return password
-
-def validate(user = {}):
-    
-    return user
