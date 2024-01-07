5db7e594daae71a429d5e78a9e46e7567ef5768d
Guillermo Reyes <guillermo_hard@hotmail.com>
2024-01-07 01:54:25 -0600

    add user_transactions

--- config/belvo.py
+++ config/belvo.py
@@ -3,10 +3,18 @@ import os
 
 class BelvoManager():
     url_base: str = os.getenv('BELVO_URL_BASE', 'https://sandbox.belvo.com') 
-    token: str = os.getenv('BELVO_TOKEN','')
+    token: str = os.getenv('BELVO_TOKEN','ZDQ0ZTZkYTQtZDdhOS00OThmLWI3MWMtMzA3YTQ1NDY4NmJjOnlSMWdXMWVvR2N3RXFWeDkqI2FEUjVibVE2RUNXVVo1RmhkMyNtZm9jQWJoMzkjbl9ud2hhdDA2TkBHVlZAI3Y=')
     
     def get_users(self, page:int = 1):
         url = f"{self.url_base}/api/owners/?page={page}"
+        headers = {
+            'Authorization': f'Basic {self.token}'
+        }
+        response = request("GET", url, headers=headers)
+        return response.json()
+    
+    def get_transactions(self, page:int = 1, user:str = ""):
+        url = f"{self.url_base}/api/transactions/?page={page}&link={user}"
         headers = {
             'Authorization': f'Basic {self.token}'
         }
