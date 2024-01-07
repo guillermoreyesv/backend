e87c6ba75b4930249305d2acaed15febdacdfe46
Guillermo Reyes <guillermo_hard@hotmail.com>
2024-01-07 01:05:32 -0600

    token

--- config/belvo.py
+++ config/belvo.py
@@ -3,7 +3,7 @@ import os
 
 class BelvoManager():
     url_base: str = os.getenv('BELVO_URL_BASE', 'https://sandbox.belvo.com') 
-    token: str = os.getenv('BELVO_TOKEN','ZDQ0ZTZkYTQtZDdhOS00OThmLWI3MWMtMzA3YTQ1NDY4NmJjOnlSMWdXMWVvR2N3RXFWeDkqI2FEUjVibVE2RUNXVVo1RmhkMyNtZm9jQWJoMzkjbl9ud2hhdDA2TkBHVlZAI3Y=')
+    token: str = os.getenv('BELVO_TOKEN','')
     
     def get_users(self, page:int = 1):
         url = f"{self.url_base}/api/owners/?page={page}"
